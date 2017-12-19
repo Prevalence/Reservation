c16ae50d550e643c7326e032106c0c7dffc77ef7
Prevalence <945926945@qq.com>
2017-12-19 14:40:21 +0800

    修正注释

--- src/main/java/user/LoginServlet.java
+++ src/main/java/user/LoginServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpSession;
 import util.DBManager;
 
 /**
- * Servlet implementation class UserServlet
+ * Servlet implementation class LoginServlet
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
