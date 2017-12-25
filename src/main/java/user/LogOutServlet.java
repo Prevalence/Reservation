49170ff82dd4133f66c8b03e1400a5daf5b092ed
Prevalence <945926945@qq.com>
2017-12-25 20:15:37 +0800

    完善logout的跳转

--- src/main/java/user/LogOutServlet.java
+++ src/main/java/user/LogOutServlet.java
@@ -26,6 +26,7 @@ public class LogOutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.getSession().invalidate();
+		response.sendRedirect("login.jsp");
 	}
 
 	/**
