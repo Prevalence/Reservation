5baa85975b23f6b054e16ebf0851fc8b30b3bd1d
Prevalence <945926945@qq.com>
2018-01-06 20:29:06 +0800

    小修改

--- src/main/java/servlet/OrdersServlet.java
+++ src/main/java/servlet/OrdersServlet.java
@@ -71,7 +71,6 @@ public class OrdersServlet extends HttpServlet {
 			 * 根据当前页号，绑定子List到Session上
 			 */
 			session.setAttribute("orderList", allOrders.subList(PAGE_SIZE * (pageNow - 1), PAGE_SIZE * pageNow));
-			System.out.println(allOrders.size());
 			request.getRequestDispatcher("order.jsp").forward(request, response);
 
 		}
