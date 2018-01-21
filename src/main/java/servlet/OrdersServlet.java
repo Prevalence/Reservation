afb3d6ca77477516c88ac42abf7df14f4c014eb6
lianyouCat <945926945@qq.com>
2018-01-21 17:00:05 +0800

    完成实验七（作业八）集成Hibernate与Spring

--- src/main/java/servlet/OrdersServlet.java
+++ src/main/java/servlet/OrdersServlet.java
@@ -25,6 +25,9 @@ import service.OrderService;
  */
 public class OrdersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	/**
+	 * 业务逻辑层对象
+	 */
 	private static OrderService orderService;
 
 	@Override
--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
